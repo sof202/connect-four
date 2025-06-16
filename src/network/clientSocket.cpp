78d549cd850d890a2564e1613344cc8e29eaee5e
sof202 <s.o.fletcher@exeter.ac.uk>
2025-06-16 21:14:31 +0100

    feat: add a message type enum with stringview conversions
    This will clear up conditional logic in handling client messages

--- src/network/clientSocket.cpp
+++ src/network/clientSocket.cpp
@@ -4,12 +4,46 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+#include <array>
 #include <cerrno>
+#include <cstdint>
 #include <cstring>
 #include <stdexcept>
 #include <string>
+#include <string_view>
 
 #include "network/address.hpp"
+
+namespace MessageType {
+enum Type : std::uint8_t {
+   move,
+   board,
+   requestInput,
+   end,
+   info,
+   maxMessageType,
+};
+using namespace std::string_view_literals;
+constexpr std::array message_type_names{
+    "move"sv,
+    "board"sv,
+    "requestInput"sv,
+    "end"sv,
+    "info"sv,
+};
+static_assert(message_type_names.size() ==
+              static_cast<std::size_t>(maxMessageType));
+constexpr auto getTypeName(const Type& type) -> std::string_view {
+   return message_type_names.at(static_cast<std::size_t>(type));
+}
+constexpr auto getTypeFromName(std::string_view type_name) -> Type {
+   for (std::size_t idx{0}; idx < maxMessageType; ++idx) {
+      if (message_type_names.at(idx) == type_name)
+         return static_cast<Type>(idx);
+   }
+   return info;
+}
+}  // namespace MessageType
 
 void ClientSocket::connectToServer(const IPv4Address& server_address) {
    if (connect(socketDescriptor(), server_address, server_address.length()) <
