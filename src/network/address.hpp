af59493a1f67c36658cb1e5953b0abca97974375
sof202 <s.o.fletcher@exeter.ac.uk>
2025-06-22 13:06:19 +0100

    feat: add exception for socket connection error

--- src/network/address.hpp
+++ src/network/address.hpp
@@ -6,6 +6,7 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 
+#include <cstdint>
 #include <cstring>
 #include <stdexcept>
 #include <string>
@@ -13,14 +14,20 @@
 class IPv4Address {
   public:
    IPv4Address() = default;
-   explicit IPv4Address(const std::string& ip_address, uint16_t port) {
+   explicit IPv4Address(const std::string& ip_address, uint16_t port) :
+       m_ip_address{ip_address},
+       m_port{port} {
       if (inet_pton(static_cast<sa_family_t>(AF_INET),
                     ip_address.c_str(),
                     &m_address.sin_addr) != 1)
          throw std::runtime_error("Invalid IPv4 address: " + ip_address);
       m_address.sin_port = htons(port);
    }
-   explicit IPv4Address(uint16_t port) { m_address.sin_port = htons(port); }
+   explicit IPv4Address(uint16_t port) :
+       m_ip_address{"0.0.0.0"},
+       m_port{port} {
+      m_address.sin_port = htons(port);
+   }
 
    // NOLINTBEGIN(cppcoreguidelines-pro-type-reinterpret-cast)
    // reinterpret cast is expected behaviour here (Thanks POSIX)
@@ -30,9 +37,14 @@ class IPv4Address {
    }
    // NOLINTEND(cppcoreguidelines-pro-type-reinterpret-cast)
 
+   [[nodiscard]] auto addressString() const -> std::string {
+      return m_ip_address + ':' + std::to_string(m_port);
+   }
    [[nodiscard]] auto length() const -> socklen_t { return sizeof(m_address); }
 
   private:
+   std::string m_ip_address;
+   uint16_t m_port{};
    sockaddr_in m_address{
        .sin_family = AF_INET, .sin_port = 0, .sin_addr = {}, .sin_zero = {}};
 };
--- src/network/networkException.hpp
+++ src/network/networkException.hpp
@@ -4,6 +4,8 @@
 #include <stdexcept>
 #include <string>
 
+#include "network/address.hpp"
+
 class NetworkException : public std::runtime_error {
   public:
    explicit NetworkException(const std::string& message) :
@@ -14,6 +16,13 @@ class SocketDisconnectException : public NetworkException {
   public:
    explicit SocketDisconnectException() :
        NetworkException("Socket was disconnected") {}
+};
+
+class SocketConnectionException : public NetworkException {
+  public:
+   explicit SocketConnectionException(const IPv4Address& address) :
+       NetworkException("Socket failed to connect to " +
+                        address.addressString()) {}
 };
 
 #endif  // NETWORK_EXCEPTION_H_
